2a7709ca29672c13b5b4c9a0c27059676cc3ec5e
Niccolò Vianello <vianello.niccolo@gmail.com>
2019-03-26 16:13:19 +0100

    doCercaCorsi() fatto

--- src/it/polito/tdp/corsi/Main.java
+++ src/it/polito/tdp/corsi/Main.java
@@ -1,21 +1,27 @@
 package it.polito.tdp.corsi;
-	
+
+import it.polito.tdp.corsi.model.GestoreCorsi;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("GestoreCorsi.fxml"));
-			Scene scene = new Scene(root,400,400);
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("GestoreCorsi.fxml"));
+			BorderPane root = (BorderPane)loader.load();
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			GestoreCorsi model = new GestoreCorsi();
+			GestoreCorsiController controller = loader.getController();
+			controller.setModel(model);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
